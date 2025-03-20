574e7eaa4d4537ea1c3ada60f152cee877f74f9e
valeriya <valeriyastoyan26@gmail.com>
2025-03-20 15:47:09 +0300

    create getNews func

--- news-app/src/pages/Main/Main.jsx
+++ news-app/src/pages/Main/Main.jsx
@@ -1,7 +1,23 @@
+import { useEffect } from 'react';
 import NewsBanner from '../../components/NewsBanner/NewsBanner';
 import styles from './styles.module.css';
+import { getNews } from '../../api/apiNews';
 
 const Main = () => {
+
+useEffect(() => {
+    const fetchNews = async() => {
+        try{
+            const news = await getNews();
+            console.log(news);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+    fetchNews();
+}, [])
+
     return (
         <main className={styles.main}>
             <NewsBanner/>
