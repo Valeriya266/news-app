e1d10361a78281a40d49e2a87fcc5b1cd6317e73
valeriya <valeriyastoyan26@gmail.com>
2025-03-20 18:30:25 +0300

    corr

--- news-app/src/pages/Main/Main.jsx
+++ news-app/src/pages/Main/Main.jsx
@@ -11,7 +11,6 @@ const Main = () => {
         const fetchNews = async() => {
             try {
                 const response = await getNews();
-                console.log(response);
                 setNews(response.news);
             }
             catch (error) {
